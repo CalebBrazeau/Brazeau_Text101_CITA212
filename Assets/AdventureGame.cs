a26f092a2d00f1ecd32e52147cfe74286389551c
DontWorryAboutIt <brazea435@morrisville.edu>
2020-02-11 15:32:43 -0500

    Update Text Component

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Text Component available in inspector
+    [SerializeField] Text txtComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtComponent.text = ("I am a string added programmatically");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
